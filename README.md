ebb82f00e987cad82b8f86b19ba179bd13856a38
Carlos <carlos.oli.h23@gmail.com>
2025-03-15 23:26:01 -0300

    Commit inicial com resolução do primeiro exercício.

---
+++ README.md
@@ -0,0 +1,2 @@
+Resolução de exercícios de lógica de programa em Java
+[Exercícios desse link!](https://www.dio.me/articles/lista-de-exercicios-para-treinar-logica-de-programacao)
---
+++ src/exercicio1/resolucao1.java
@@ -0,0 +1,30 @@
+package exercicio1;
+
+import java.util.Scanner;
+// 1 - Faça um algoritmo que leia os valores de A, B, C e em seguida imprima na tela a soma entre A e B é mostre se a soma é menor que C.
+
+public class resolucao1 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        double a,b,c,soma;
+
+        System.out.println("-> Comparador de uma Soma com um Número <-");
+        System.out.println("Os dois primeiro números digitados serão somados e comparados com o terceiro número.\n");
+
+        System.out.print("Digite o primeiro valor: ");
+        a = meuScanner.nextDouble();
+        System.out.print("Digite o segundo valor: ");
+        b = meuScanner.nextDouble();
+        System.out.print("Digite o último valor: ");
+        c = meuScanner.nextDouble();
+
+        soma = a+b;
+        System.out.printf("\nA soma entre os valores %.0f e %.0f resulta em %.0f.\n\n",a,b,soma);
+
+        if(soma >= c) {
+            System.out.print("A soma não é menor que C.");
+        } else {
+            System.out.print("A soma é menor que C!");
+        }
+    }
+}
