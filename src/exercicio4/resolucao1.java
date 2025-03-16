7618f41dc09ba7c80474b180b7626f8a2387cf85
Carlos <carlos.oli.h23@gmail.com>
2025-03-15 23:29:52 -0300

    Commit exercício 4.

---
+++ src/exercicio4/resolucao1.java
@@ -0,0 +1,19 @@
+package exercicio4;
+
+import java.util.Scanner;
+
+//4 - Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
+
+public class resolucao1 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        int num;
+
+        System.out.print("Digite um número: ");
+        num = meuScanner.nextInt();
+        num = num-1;
+
+        System.out.print("Antecessor do número: "+ num++);
+        System.out.print("\nSucessor do número: "+ ++num);
+    }
+}
