a69293af2be02d82525d0c11146f572eee6afc1e
Carlos <carlos.oli.h23@gmail.com>
2025-03-16 00:02:40 -0300

    Commit exercícios 9 e 10.

---
+++ src/exercicio10/Resolucao.java
@@ -0,0 +1,22 @@
+package exercicio10;
+
+import java.util.Scanner;
+
+//10 - Faça um algoritmo que leia três notas obtidas por um aluno, e imprima na tela a média das notas.
+
+public class Resolucao {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        double nota1,nota2,nota3,media;
+
+        System.out.print("Digite a primeira nota: ");
+        nota1 = meuScanner.nextDouble();
+        System.out.print("Digite a segunda nota: ");
+        nota2 = meuScanner.nextDouble();
+        System.out.print("Digite a terceira nota: ");
+        nota3 = meuScanner.nextDouble();
+
+        media = (nota1+nota2+nota3)/3;
+        System.out.printf("A média das três notas é %.1f",media);
+    }
+}
--- src/exercicio9/Resolucao.java
+++ src/exercicio9/Resolucao.java
@@ -1,4 +1,45 @@
 package exercicio9;
 
+import java.util.Scanner;
+
+/*9 - Faça um algoritmo que calcule o IMC (Índice de Massa Corporal) de uma pessoa, leia o seu peso e sua altura e imprima na tela sua condição
+de acordo com a tabela abaixo:
+
+Fórmula do IMC = peso / (altura) ²
+
+Tabela Condições IMC
+
+ Abaixo de 18,5   | Abaixo do peso
+ Entre 18,6 e 24,9 | Peso ideal (parabéns)
+ Entre 25,0 e 29,9 | Levemente acima do peso
+ Entre 30,0 e 34,9 | Obesidade grau I
+ Entre 35,0 e 39,9 | Obesidade grau II (severa)
+ Maior ou igual a 40 | Obesidade grau III (mórbida) */
+
 public class Resolucao {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        double altura, peso, valorIMC;
+
+        System.out.print("Digite sua altura: ");
+        altura = meuScanner.nextDouble();
+        System.out.print("Digite seu peso: ");
+        peso = meuScanner.nextDouble();
+
+        valorIMC = peso / (altura*altura);
+
+        if(valorIMC < 18.5) {
+            System.out.print("Abaixo do peso.");
+        } else if(valorIMC >= 18.5 && valorIMC <25) {
+            System.out.print("Peso ideal.");
+        } else if(valorIMC >= 25 && valorIMC <30) {
+            System.out.print("Levemente acima do peso.");
+        } else if(valorIMC >= 30 && valorIMC <35) {
+            System.out.print("Obesidade grau I.");
+        } else if(valorIMC >= 35 && valorIMC <40) {
+            System.out.print("Obesidade grau II (severa).");
+        } else if(valorIMC>=40){
+            System.out.print("Obesidade grau III (mórbida).");
+        }
+    }
 }
