306c63bfd41c0e625670dff43951d08330d1fafb
Carlos <carlos.oli.h23@gmail.com>
2025-03-15 23:31:37 -0300

    Commit das duas resoluções do exercício 8.

---
+++ src/exercicio8/resolucao1.java
@@ -0,0 +1,41 @@
+package exercicio8;
+
+import java.util.Scanner;
+
+//8 - Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
+
+public class resolucao1 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("Digite o primeiro valor: ");
+        a = meuScanner.nextInt();
+        System.out.print("Digite o segundo valor: ");
+        b = meuScanner.nextInt();
+        System.out.print("Digite o terceiro valor: ");
+        c = meuScanner.nextInt();
+
+        if (a < b && a < c) {
+            if (b < c) {
+                System.out.print("Ordem decrescente: " + a + "," + b + "," + c + ".");
+            } else {
+                System.out.print("Ordem decrescente: " + a + "," + c + "," + b + ".");
+            }
+        } else if (b < a && b < c) {
+            if (a < c) {
+                System.out.print("Ordem decrescente: " + b + "," + a + "," + c + ".");
+            } else {
+                System.out.print("Ordem decrescente: " + b + "," + c + "," + a + ".");
+            }
+        } else if (c < a && c < b) {
+            if (b < a) {
+                System.out.print("Ordem decrescente: " + c + "," + b + "," + a + ".");
+            } else {
+                System.out.print("Ordem decrescente: " + c + "," + a + "," + b + ".");
+            }
+        } else {
+            System.out.println("\nPor favor, não digite valores iguais.");
+        }
+    }
+}
---
+++ src/exercicio8/resolucao2.java
@@ -0,0 +1,57 @@
+package exercicio8;
+
+import java.util.Scanner;
+
+//8 - Faça um algoritmo que leia três valores inteiros diferentes e imprima na tela os valores em ordem decrescente.
+
+public class resolucao2 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        int a, b, c;
+        int menor = 0;
+        int meio = 0;
+        int maior = 0;
+
+        System.out.print("Digite o primeiro valor: ");
+        a = meuScanner.nextInt();
+        System.out.print("Digite o segundo valor: ");
+        b = meuScanner.nextInt();
+        System.out.print("Digite o terceiro valor: ");
+        c = meuScanner.nextInt();
+
+        if(a<b && a<c) {
+            menor = a;
+            if (b<c) {
+                meio = b;
+                maior = c;
+            } else {
+                meio = c;
+                maior = b;
+            }
+        } else if(b<a && b<c) {
+            menor = b;
+            if(a<c) {
+                meio = a;
+                maior = c;
+            } else {
+                meio = c;
+                maior = a;
+            }
+        } else if(c<a && c<b) {
+            menor = c;
+            if(b<a) {
+                meio = b;
+                maior = a;
+            } else {
+                meio = a;
+                maior = b;
+            }
+        } else {
+            System.out.println("\nPor favor, não digite valores iguais.");
+        }
+
+        System.out.println("A ordem decrescente é: "+menor+","+meio+","+maior+".");
+        //Essa alternativa de resolução diminui a quantidade de impressões no código, mas não sei se o código fica visivelmente melhor.
+    }
+}
+
