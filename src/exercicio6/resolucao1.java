9f0ac3b8e9ec6736f13e41da1ac85246b3c10589
Carlos <carlos.oli.h23@gmail.com>
2025-03-15 23:30:41 -0300

    Commit exercício 6.

---
+++ src/exercicio6/resolucao1.java
@@ -0,0 +1,18 @@
+package exercicio6;
+
+import java.util.Scanner;
+
+//6 - Faça um algoritmo que leia um valor qualquer e imprima na tela com um reajuste de 5%.
+
+public class resolucao1 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        double valor;
+
+        System.out.print("Digite um valor qualquer: ");
+        valor = meuScanner.nextDouble();
+
+        valor += (valor*5)/100;
+        System.out.println("Valor com reajuste "+valor);
+    }
+}
