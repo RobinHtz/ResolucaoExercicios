703c3013c667c1712a57fcea9e285ce3d1199f71
Robin <149825242+RobinHtz@users.noreply.github.com>
2025-03-16 00:05:30 -0300

    Update and rename resolucao1.java to Resolucao.java

--- src/exercicio5/Resolucao.java
+++ src/exercicio5/Resolucao.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /* 5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse
 usuário ganha e imprima na tela o resultado. (Base para o Salário mínimo R$ 1.293,20).  */
 
-public class resolucao1 {
+public class Resolucao {
     public static void main(String[] args) {
         Scanner meuScanner = new Scanner(System.in);
         double salarioMinimo,salarioUsuario,quantidadeSalariosMin;
