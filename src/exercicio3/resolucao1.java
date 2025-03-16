59f5f56013d52070c68476a1ef60e9a7d63f8539
Carlos <carlos.oli.h23@gmail.com>
2025-03-15 23:29:26 -0300

    Commit do exercício 3.

---
+++ src/exercicio3/resolucao1.java
@@ -0,0 +1,28 @@
+package exercicio3;
+
+import java.util.Scanner;
+
+/* 3 - Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores,
+caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e
+imprimir seu valor na tela.
+*/
+
+public class resolucao1 {
+    public static void main(String[] args) {
+        Scanner meuScanner = new Scanner(System.in);
+        int A,B,C;
+
+        System.out.print("Digite o primeiro número: ");
+        A = meuScanner.nextInt();
+        System.out.print("Digite o segundo número: ");
+        B = meuScanner.nextInt();
+
+        if(A==B) {
+            C=A+B;
+        } else {
+            C=A*B;
+        }
+
+        System.out.print("\nO resultado é "+C);
+    }
+}
