fffaf04ac5795711015895946bf2cae2bd075b5d
Carlos <carlos.oli.h23@gmail.com>
2025-03-31 19:05:04 -0300

    Enviando exercício 15

--- src/exercicio_15/Resolucao.java
+++ src/exercicio_15/Resolucao.java
@@ -8,6 +8,23 @@ consideração o ano com 365 dias e o mês com 30 dias.
 
 public class Resolucao {
     public static void main(String[] args) {
+        int anoNascimento = lerAnoNascimento();
+        calcular(anoNascimento);
+    }
+
+    public static int lerAnoNascimento() {
         Scanner meuScanner = new Scanner(System.in);
+
+        System.out.print("Digite o ano de nascimento: ");
+        return meuScanner.nextInt();
+    }
+
+    public static void calcular(int anoNascimento) {
+        int anos = 2025 - anoNascimento;
+        int meses = anos * 12;
+        int dias = anos * 365;
+
+        System.out.println("Você viveu:");
+        System.out.println(anos + " anos, " + meses + " meses e " + dias + " dias.");
     }
 }
